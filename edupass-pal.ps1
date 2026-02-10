ec075d092b23556d2d19dfdacdb4ffcd0a2d19ab
elekram <mlee.aus@gmail.com>
2026-02-10 16:07:48 +1100

    create cookie container and login to stmc console

---
+++ edupass-pal.ps1
@@ -0,0 +1,43 @@
+$config = Get-Content -Raw -Path .\config\config.json | ConvertFrom-Json
+
+
+
+function Main {
+  $username = $config.Credentials.user
+  $password = $config.Credentials.password
+
+  $pair = "$username`:$password"
+  $bytes = [System.Text.Encoding]::ASCII.GetBytes($pair)
+  $encoded = [Convert]::ToBase64String($bytes)
+  
+
+  $cookieContainer = New-Object System.Net.CookieContainer
+  $handler = New-Object System.Net.Http.HttpClientHandler
+  $handler.CookieContainer = $cookieContainer
+  $handler.UseCookies = $true
+
+  $handler.Credentials = New-Object System.Net.NetworkCredential(
+    $username,
+    $password
+  )
+
+  $c = New-Object System.Net.Http.HttpClient($handler)
+  $c.DefaultRequestHeaders.Add("User-Agent", "PowerShell-HttpClient")
+
+  Write-Host "Connecting to stmc.education.vic.gov.au..."
+  $response = $c.GetAsync("https://stmc.education.vic.gov.au/stud_pwd").Result
+
+
+  if (-not $response.IsSuccessStatusCode) {
+    Write-Host "Request Failed: login"
+    Write-Host ($response.StatusCode)
+    Write-Host ([int]$response.StatusCode)
+  }
+
+  Write-Host "Ok"
+
+
+}
+
+
+Main
